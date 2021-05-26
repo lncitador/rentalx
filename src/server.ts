c75729ec3877eed66177f7e4111629c938e6a059
lncitador <walaffonofre@icloud.com>
2021-05-26 00:56:44 -0300

    🎉 starting a new journey

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.routes";
+
+const app = express();
+
+app.use(express.json());
+app.use("/categories", categoriesRoutes);
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Ignite" });
+});
+
+app.listen(3333, () => console.log("Server Online 🚀"));
